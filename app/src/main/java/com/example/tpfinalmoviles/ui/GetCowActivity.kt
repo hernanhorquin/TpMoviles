13b12fc5f45cb851270f3b2d0a03b32db1d2a5aa
Tomás Covelli <tomas@MacBook-Pro-de-Tomas.local>
2021-06-21 20:55:42 -0300

    Merge conflicts resueltos

--- app/src/main/java/com/example/tpfinalmoviles/ui/GetCowActivity.kt
+++ app/src/main/java/com/example/tpfinalmoviles/ui/GetCowActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
